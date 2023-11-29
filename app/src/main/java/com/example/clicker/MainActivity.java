11da97a6ef782ce9b3c830618d701be42d9e798c
zhangkw <kzhang04@bu.edu>
2023-11-28 23:14:13 -0500

    created a shop page (not linked yet) and a working upgrade button (might not work after its moved but it worked when it was on the main page)

--- app/src/main/java/com/example/clicker/MainActivity.java
+++ app/src/main/java/com/example/clicker/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.clicker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private TextView title, points;
+    private Button clickbutton, upgrade;
+    double score = 0;
+    double multiplier = 1.0;
+    double cost = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        title = (TextView)findViewById(R.id.title);
+        points = (TextView)findViewById(R.id.points);
+
+        clickbutton = (Button)findViewById(R.id.clickbutton);
+
+        clickbutton.setEnabled(true);
+        upgrade.setEnabled(true);
+
+        clickbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                score += multiplier;
+                points.setText(String.format("%.2f", score)  + " Dining Points");
+            }
+        });
+
+        upgrade.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (score >= cost) {
+                    score -= cost;
+                    points.setText(String.format("%.2f", score) + " Dining Points");
+                    multiplier += 0.1;
+                    cost *= 1.1;
+                }
+            }
+        });
+
+    }
+
+    private void launchShopActivity(double score) {
+        Intent shopActivity = new Intent(MainActivity.this, Shop.class);
     }
 }
---
+++ app/src/main/java/com/example/clicker/Shop.java
@@ -0,0 +1,12 @@
+package com.example.clicker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+public class Shop extends Activity {
+    private TextView name1, name2;
+    private Button buy1, buy2;
+}
