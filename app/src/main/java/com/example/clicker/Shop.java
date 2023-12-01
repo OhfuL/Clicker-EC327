038a10ad271c03c00a2cf7ca8024e07425d82927
mbarany20 <mbarany20@gmail.com>
2023-12-01 18:06:42 -0500

    Altered Buy to Buybuilding and Name1 to Building 1 for all 1-11

--- app/src/main/java/com/example/clicker/Shop.java
+++ app/src/main/java/com/example/clicker/Shop.java
@@ -10,9 +10,10 @@ import android.widget.Toast;
 
 public class Shop extends Activity {
 
-    private TextView name1, name2, name3, name4, name5, name6, name7, name8, name9, name10, name11;
+    private TextView building1, building2, building3, building4, building5, building6, building7, buidling8, building9, buidling10, building11;
+    private Button buy_building1, buy_building2, buy_building3, buy_building4, buy_building5, buy_building6, buy_building7, buy_building8, buy_building9, buy_building10, buy_building11;
     private TextView points;
-    private Button back, buy1, buy2, buy3;
+    private Button back;
     private double score, multiplier;
     private double[] costs;
     private int[] levels;
@@ -24,30 +25,30 @@ public class Shop extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.shop);
 
-        name1 = findViewById(R.id.name1);
-        name2 = findViewById(R.id.name2);
-        name3 = findViewById(R.id.name3);
-        name4 = findViewById(R.id.name4);
-        name5 = findViewById(R.id.name5);
-        name6 = findViewById(R.id.name6);
-        name7 = findViewById(R.id.name7);
-        name8 = findViewById(R.id.name8);
-        name9 = findViewById(R.id.name9);
-        name10 = findViewById(R.id.name10);
-        name11 = findViewById(R.id.name11);
+        building1 = findViewById(R.id.building1);
+        building2 = findViewById(R.id.building2);
+        building3 = findViewById(R.id.building3);
+        building4 = findViewById(R.id.building4);
+        building5 = findViewById(R.id.building5);
+        building6 = findViewById(R.id.building6);
+        building7 = findViewById(R.id.building7);
+        buidling8 = findViewById(R.id.buidling8);
+        building9 = findViewById(R.id.building9);
+        buidling10 = findViewById(R.id.buidling10);
+        buildding11 = findViewById(R.id.buildding11);
         points = findViewById(R.id.points);
         back = findViewById(R.id.back);
-        buy1 = findViewById(R.id.buy1);
-        buy2 = findViewById(R.id.buy2);
-        buy3 = findViewById(R.id.buy3);
-        buy4 = findViewById(R.id.buy4);
-        buy5 = findViewById(R.id.buy5);
-        buy6 = findViewById(R.id.buy6);
-        buy7 = findViewById(R.id.buy7);
-        buy8 = findViewById(R.id.buy8);
-        buy9 = findViewById(R.id.buy9);
-        buy10 = findViewById(R.id.buy10);
-        buy11 = findViewById(R.id.buy11);
+        buy_building1 = findViewById(R.id.buy_building1);
+        buy_building2 = findViewById(R.id.buy_building2);
+        buy_building3 = findViewById(R.id.buy_building3);
+        buy_building4 = findViewById(R.id.buy_building4);
+        buy_building5 = findViewById(R.id.buy_building5);
+        buy_building6 = findViewById(R.id.buy_building6);
+        buy_building7 = findViewById(R.id.buy_building7);
+        buy_building8 = findViewById(R.id.buy_building8);
+        buy_building9 = findViewById(R.id.buy_building9);
+        buy_building10 = findViewById(R.id.buy_building10);
+        buy_building11 = findViewById(R.id.buy_building11);
 
         // Retrieve data from the intent
         Intent intent = getIntent();
@@ -59,10 +60,10 @@ public class Shop extends Activity {
         buildingLevels = intent.getIntArrayExtra(MainActivity.TAG_BUILDING_LEVELS);
 
         points.setText(String.format("%.2f", score) + " Dining Points");
-        name1.setText("Upgrade 1 (" + levels[0] + "x)");
-        name2.setText("Upgrade 2 (" + levels[1] + "x)");
-        buy1.setText("" + String.format("%.2f", costs[0]));
-        buy2.setText("" + String.format("%.2f", costs[1]));
+        building1.setText("Upgrade 1 (" + levels[0] + "x)");
+        building2.setText("Upgrade 2 (" + levels[1] + "x)");
+        buy_building1.setText("" + String.format("%.2f", costs[0]));
+        buy_building2.setText("" + String.format("%.2f", costs[1]));
 
         View.OnClickListener buyButtonClickListener = new View.OnClickListener() {
             @Override
@@ -71,17 +72,17 @@ public class Shop extends Activity {
             }
         };
 
-        buy1.setOnClickListener(buyButtonClickListener);
-        buy2.setOnClickListener(buyButtonClickListener);
-        buy3.setOnClickListener(buyButtonClickListener);
-        buy4.setOnClickListener(buyButtonClickListener);
-        buy5.setOnClickListener(buyButtonClickListener);
-        buy6.setOnClickListener(buyButtonClickListener);
-        buy7.setOnClickListener(buyButtonClickListener);
-        buy8.setOnClickListener(buyButtonClickListener);
-        buy9.setOnClickListener(buyButtonClickListener);
-        buy10.setOnClickListener(buyButtonClickListener);
-        buy11.setOnClickListener(buyButtonClickListener);
+        buy_building1.setOnClickListener(buyButtonClickListener);
+        buy_building2.setOnClickListener(buyButtonClickListener);
+        buy_building3.setOnClickListener(buyButtonClickListener);
+        buy_building4.setOnClickListener(buyButtonClickListener);
+        buy_building5.setOnClickListener(buyButtonClickListener);
+        buy_building6.setOnClickListener(buyButtonClickListener);
+        buy_building7.setOnClickListener(buyButtonClickListener);
+        buy_building8.setOnClickListener(buyButtonClickListener);
+        buy_building9.setOnClickListener(buyButtonClickListener);
+        buy_building10.setOnClickListener(buyButtonClickListener);
+        buy_building11.setOnClickListener(buyButtonClickListener);
 
         back.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -98,31 +99,31 @@ public class Shop extends Activity {
     }
 
     private void handleBuyButtonClick(View v) {
-        if (v == buy1) {
-            handleBuy1();
-        } else if (v == buy2) {
-            handleBuy2();
-        } else if (v == buy3) {
-            handleBuy3();
-        } else if (v == buy4) {
-            handleBuy4();
-        } else if (v == buy5) {
-            handleBuy5();
-        } else if (v == buy6) {
-            handleBuy6();
-        } else if (v == buy7) {
-            handleBuy7();
-        } else if (v == buy8) {
-            handleBuy8();
-        } else if (v == buy9) {
-            handleBuy9();
-        } else if (v == buy10) {
-            handleBuy10();
-        } else if (v == buy11) {
-            handleBuy11();
-    }
+        if (v == buy_building1) {
+            handlebuy_building1();
+        } else if (v == buy_building2) {
+            handlebuy_building2();
+        } else if (v == buy_building3) {
+            handlebuy_building3();
+        } else if (v == buy_building4) {
+            handlebuy_building4();
+        } else if (v == buy_building5) {
+            handlebuy_building5();
+        } else if (v == buy_building6) {
+            handlebuy_building6();
+        } else if (v == buy_building7) {
+            handlebuy_building7();
+        } else if (v == buy_building8) {
+            handlebuy_building8();
+        } else if (v == buy_building9) {
+            handlebuy_building9();
+        } else if (v == buy_building10) {
+            handlebuy_building10();
+        } else if (v == buy_building11) {
+            handlebuy_building11();
+        }
 
-    private void handleBuy1() {
+        private void handlebuy_building1() {
         }
         // Check if the player has enough score to make the purchase
         if (score >= costs[0]) {
@@ -135,150 +136,150 @@ public class Shop extends Activity {
             // Increase the cost of the item by 15%
             costs[0] *= 1.15;
             // Update the text on the button to display the new cost
-            buy1.setText("" + String.format("%.2f", costs[0]));
+            buy_building1.setText("" + String.format("%.2f", costs[0]));
             // Increase the level of the upgrade by 1
             levels[0]++;
             // Update the text on the label to display the new level
-            name1.setText("Upgrade 1 (" + levels[0] + "x)");
+            building1.setText("Upgrade 1 (" + levels[0] + "x)");
         } else {
             // Display a toast message indicating that the player doesn't have enough score
             Toast.makeText(Shop.this, "Not enough Dining Points!", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private void handleBuy2() {
+    private void handlebuy_building2() {
         if (score >= costs[1]) {
             score -= costs[1];
             points.setText(String.format("%.2f", score) + " Dining Points");
             multiplier += 0.1;
             costs[1] *= 1.15;
-            buy2.setText("" + String.format("%.2f", costs[1]));
+            buy_building2.setText("" + String.format("%.2f", costs[1]));
             levels[1]++;
-            name2.setText("Upgrade 2 (" + levels[1] + "x)");
+            building2.setText("Upgrade 2 (" + levels[1] + "x)");
         } else {
             Toast.makeText(Shop.this, "Not enough Dining Points!", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private void handleBuy3() {
+    private void handlebuy_building3() {
         if (score >= costs[2]) {
             score -= costs[2];
             points.setText(String.format("%.2f", score) + " Dining Points");
             multiplier += 0.1;
             costs[2] *= 1.15;
-            buy3.setText("" + String.format("%.2f", costs[2]));
+            buy_building3.setText("" + String.format("%.2f", costs[2]));
             levels[2]++;
-            name3.setText("Upgrade 3 (" + levels[2] + "x)");
+            building3.setText("Upgrade 3 (" + levels[2] + "x)");
         } else {
             Toast.makeText(Shop.this, "Not enough Dining Points!", Toast.LENGTH_SHORT).show();
         }
 
-        private void handleBuy4() {
+        private void handlebuy_building4() {
             if (score >= costs[3]) {
                 score -= costs[3];
                 points.setText(String.format("%.2f", score) + " Dining Points");
                 multiplier += 0.1;
                 costs[3] *= 1.15;
-                buy4.setText("" + String.format("%.2f", costs[3]));
+                buy_building4.setText("" + String.format("%.2f", costs[3]));
                 levels[3]++;
-                name4.setText("Upgrade 4 (" + levels[3] + "x)");
+                building4.setText("Upgrade 4 (" + levels[3] + "x)");
             } else {
                 Toast.makeText(Shop.this, "Not enough Dining Points!", Toast.LENGTH_SHORT).show();
             }
         }
 
-        private void handleBuy5() {
+        private void handlebuy_building5() {
             if (score >= costs[4]) {
                 score -= costs[4];
                 points.setText(String.format("%.2f", score) + " Dining Points");
                 multiplier += 0.1;
                 costs[4] *= 1.15;
-                buy5.setText("" + String.format("%.2f", costs[4]));
+                buy_building5.setText("" + String.format("%.2f", costs[4]));
                 levels[4]++;
-                name5.setText("Upgrade 5 (" + levels[4] + "x)");
+                building5.setText("Upgrade 5 (" + levels[4] + "x)");
             } else {
                 Toast.makeText(Shop.this, "Not enough Dining Points!", Toast.LENGTH_SHORT).show();
             }
         }
 
-        private void handleBuy6() {
+        private void handlebuy_building6() {
             if (score >= costs[5]) {
                 score -= costs[5];
                 points.setText(String.format("%.2f", score) + " Dining Points");
                 multiplier += 0.1;
                 costs[5] *= 1.15;
-                buy6.setText("" + String.format("%.2f", costs[5]));
+                buy_building6.setText("" + String.format("%.2f", costs[5]));
                 levels[5]++;
-                name6.setText("Upgrade 6 (" + levels[5] + "x)");
+                building6.setText("Upgrade 6 (" + levels[5] + "x)");
             } else {
                 Toast.makeText(Shop.this, "Not enough Dining Points!", Toast.LENGTH_SHORT).show();
             }
         }
 
-        private void handleBuy7() {
+        private void handlebuy_building7() {
             if (score >= costs[6]) {
                 score -= costs[6];
                 points.setText(String.format("%.2f", score) + " Dining Points");
                 multiplier += 0.1;
                 costs[6] *= 1.15;
-                buy7.setText("" + String.format("%.2f", costs[6]));
+                buy_building7.setText("" + String.format("%.2f", costs[6]));
                 levels[6]++;
-                name7.setText("Upgrade 7 (" + levels[6] + "x)");
+                building7.setText("Upgrade 7 (" + levels[6] + "x)");
             } else {
                 Toast.makeText(Shop.this, "Not enough Dining Points!", Toast.LENGTH_SHORT).show();
             }
         }
 
-        private void handleBuy8() {
+        private void handlebuy_building8() {
             if (score >= costs[7]) {
                 score -= costs[7];
                 points.setText(String.format("%.2f", score) + " Dining Points");
                 multiplier += 0.1;
                 costs[7] *= 1.15;
-                buy8.setText("" + String.format("%.2f", costs[7]));
+                buy_building8.setText("" + String.format("%.2f", costs[7]));
                 levels[7]++;
-                name8.setText("Upgrade 8 (" + levels[7] + "x)");
+                buidling8.setText("Upgrade 8 (" + levels[7] + "x)");
             } else {
                 Toast.makeText(Shop.this, "Not enough Dining Points!", Toast.LENGTH_SHORT).show();
             }
         }
 
-        private void handleBuy9() {
+        private void handlebuy_building9() {
             if (score >= costs[8]) {
                 score -= costs[8];
                 points.setText(String.format("%.2f", score) + " Dining Points");
                 multiplier += 0.1;
                 costs[8] *= 1.15;
-                buy9.setText("" + String.format("%.2f", costs[8]));
+                buy_building9.setText("" + String.format("%.2f", costs[8]));
                 levels[8]++;
-                name9.setText("Upgrade 9 (" + levels[8] + "x)");
+                building9.setText("Upgrade 9 (" + levels[8] + "x)");
             } else {
                 Toast.makeText(Shop.this, "Not enough Dining Points!", Toast.LENGTH_SHORT).show();
             }
         }
-        private void handleBuy10() {
+        private void handlebuy_building10() {
             if (score >= costs[9]) {
                 score -= costs[9];
                 points.setText(String.format("%.2f", score) + " Dining Points");
                 multiplier += 0.1;
                 costs[9] *= 1.15;
-                buy10.setText("" + String.format("%.2f", costs[9]));
+                buy_building10.setText("" + String.format("%.2f", costs[9]));
                 levels[9]++;
-                name10.setText("Upgrade 10 (" + levels[9] + "x)");
+                buidling10.setText("Upgrade 10 (" + levels[9] + "x)");
             } else {
                 Toast.makeText(Shop.this, "Not enough Dining Points!", Toast.LENGTH_SHORT).show();
             }
         }
 
-        private void handleBuy11() {
+        private void handlebuy_building11() {
             if (score >= costs[10]) {
                 score -= costs[10];
                 points.setText(String.format("%.2f", score) + " Dining Points");
                 multiplier += 0.1;
                 costs[10] *= 1.15;
-                buy11.setText("" + String.format("%.2f", costs[10]));
+                buy_building11.setText("" + String.format("%.2f", costs[10]));
                 levels[10]++;
-                name11.setText("Upgrade 11 (" + levels[10] + "x)");
+                buildinding11.setText("Upgrade 11 (" + levels[10] + "x)");
             } else {
                 Toast.makeText(Shop.this, "Not enough Dining Points!", Toast.LENGTH_SHORT).show();
             }
