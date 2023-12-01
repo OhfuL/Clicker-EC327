727d7014a6a6dda84b0dae54db9ddd8f902058a5
mbarany20 <mbarany20@gmail.com>
2023-12-01 17:36:30 -0500

    Addded the necessary buy3-buy11 and added the upgrade 3-11

--- app/src/main/java/com/example/clicker/MainActivity.java
+++ app/src/main/java/com/example/clicker/MainActivity.java
@@ -1,5 +1,7 @@
+// Package declaration
 package com.example.clicker;
 
+// Importing necessary classes from Android framework
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,8 +10,10 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.os.Handler;
 
+// Main activity class of the application
 public class MainActivity extends Activity {
 
+    // Constants used as keys for passing data between activities
     public static final String TAG_SCORE = "score";
     public static final String TAG_COSTS = "costs";
     public static final String TAG_MULTIPLIER = "multiplier";
@@ -17,10 +21,13 @@ public class MainActivity extends Activity {
     public static final String TAG_BUILDING_LEVELS = "buildinglevels";
     public static final String TAG_BUILDING_COSTS = "buildingcosts";
     public static final int REQUEST_CODE_SHOP = 1;
+
+
+    // UI elements and variables for game logic
     private TextView title, points;
     private Button clickbutton, shop;
     private Handler clickHandler;
-    private final int TIME_INTERVAL_MILLIS = 1000;
+    private final int TIME_INTERVAL_MILLIS = 1000; // Interval for auto-click feature
     double score = 0;
     double multiplier = 1.0;
     double[] costs = {10, 100};
@@ -29,39 +36,55 @@ public class MainActivity extends Activity {
     double[] buildingCosts = {15, 100, 1100, 12000, 130000, 1400000, 20000000, 330000000, 5100000000d, 75000000000d};
     final double[] buildingRates = {0.3, 1, 8, 47, 260, 1400, 7800, 44000, 260000, 1600000};
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // Set the layout for the activity
 
+        // Initializing UI elements
         title = (TextView)findViewById(R.id.title);
         points = (TextView)findViewById(R.id.points);
 
         clickbutton = (Button)findViewById(R.id.clickbutton);
         shop = (Button)findViewById(R.id.shop);
 
+
+        // Enabling buttons
         clickbutton.setEnabled(true);
         shop.setEnabled(true);
 
+        // Handler for timed actions
         clickHandler = new Handler();
-        autoClickTick.run();
+        autoClickTick.run(); // Start the auto-clicking
 
+
+        // Listener for the click button
         clickbutton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                score += multiplier;
-                updateScore(score);
+                score += multiplier; // Increase score on click of "rhett"
+                updateScore(score); //updates the score when pressed
             }
         });
 
+
+        // Listener for the shop button
         shop.setOnClickListener(new View.OnClickListener() {
+
+            // Create an intent to start the Shop activity
             public void onClick(View v) {
                 Intent shopActivity = new Intent(MainActivity.this, Shop.class);
+
+                // Put current game data into the intent
                 shopActivity.putExtra(TAG_SCORE, score);
                 shopActivity.putExtra(TAG_COSTS, costs);
                 shopActivity.putExtra(TAG_MULTIPLIER, multiplier);
                 shopActivity.putExtra(TAG_LEVELS, levels);
                 shopActivity.putExtra(TAG_BUILDING_COSTS, buildingCosts);
                 shopActivity.putExtra(TAG_BUILDING_LEVELS, buildingLevels);
+
+                // Start the Shop activity and wait for its result
                 startActivityForResult(shopActivity, REQUEST_CODE_SHOP);
             }
         });
@@ -71,6 +94,8 @@ public class MainActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+
+        // Handling the result returned from the Shop activity
         if (requestCode == REQUEST_CODE_SHOP && resultCode == Activity.RESULT_OK) {
             // Retrieve updated data from the intent
             score = data.getDoubleExtra(TAG_SCORE, 0);
@@ -85,14 +110,18 @@ public class MainActivity extends Activity {
         }
     }
 
+
+    // Runnable for automatic score increment
     private Runnable autoClickTick = new Runnable() {
+
+        // Increment score based on building levels and rates
         @Override
         public void run() {
             for (int i = 0; i < buildingLevels.length; ++i) {
                 score += buildingLevels[i] * buildingRates[i];
             }
-            updateScore(score);
-            clickHandler.postDelayed(this, TIME_INTERVAL_MILLIS);
+            updateScore(score); // Update the score display
+            clickHandler.postDelayed(this, TIME_INTERVAL_MILLIS); // Schedule the next run
         }
     };
 
--- app/src/main/java/com/example/clicker/Shop.java
+++ app/src/main/java/com/example/clicker/Shop.java
@@ -10,7 +10,8 @@ import android.widget.Toast;
 
 public class Shop extends Activity {
 
-    private TextView name1, name2, name3, points;
+    private TextView name1, name2, name3, name4, name5, name6, name7, name8, name9, name10, name11;
+    private TextView points;
     private Button back, buy1, buy2, buy3;
     private double score, multiplier;
     private double[] costs;
@@ -26,11 +27,27 @@ public class Shop extends Activity {
         name1 = findViewById(R.id.name1);
         name2 = findViewById(R.id.name2);
         name3 = findViewById(R.id.name3);
+        name4 = findViewById(R.id.name4);
+        name5 = findViewById(R.id.name5);
+        name6 = findViewById(R.id.name6);
+        name7 = findViewById(R.id.name7);
+        name8 = findViewById(R.id.name8);
+        name9 = findViewById(R.id.name9);
+        name10 = findViewById(R.id.name10);
+        name11 = findViewById(R.id.name11);
         points = findViewById(R.id.points);
         back = findViewById(R.id.back);
         buy1 = findViewById(R.id.buy1);
         buy2 = findViewById(R.id.buy2);
         buy3 = findViewById(R.id.buy3);
+        buy4 = findViewById(R.id.buy4);
+        buy5 = findViewById(R.id.buy5);
+        buy6 = findViewById(R.id.buy6);
+        buy7 = findViewById(R.id.buy7);
+        buy8 = findViewById(R.id.buy8);
+        buy9 = findViewById(R.id.buy9);
+        buy10 = findViewById(R.id.buy10);
+        buy11 = findViewById(R.id.buy11);
 
         // Retrieve data from the intent
         Intent intent = getIntent();
@@ -56,6 +73,15 @@ public class Shop extends Activity {
 
         buy1.setOnClickListener(buyButtonClickListener);
         buy2.setOnClickListener(buyButtonClickListener);
+        buy3.setOnClickListener(buyButtonClickListener);
+        buy4.setOnClickListener(buyButtonClickListener);
+        buy5.setOnClickListener(buyButtonClickListener);
+        buy6.setOnClickListener(buyButtonClickListener);
+        buy7.setOnClickListener(buyButtonClickListener);
+        buy8.setOnClickListener(buyButtonClickListener);
+        buy9.setOnClickListener(buyButtonClickListener);
+        buy10.setOnClickListener(buyButtonClickListener);
+        buy11.setOnClickListener(buyButtonClickListener);
 
         back.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -76,19 +102,46 @@ public class Shop extends Activity {
             handleBuy1();
         } else if (v == buy2) {
             handleBuy2();
-        }
+        } else if (v == buy3) {
+            handleBuy3();
+        } else if (v == buy4) {
+            handleBuy4();
+        } else if (v == buy5) {
+            handleBuy5();
+        } else if (v == buy6) {
+            handleBuy6();
+        } else if (v == buy7) {
+            handleBuy7();
+        } else if (v == buy8) {
+            handleBuy8();
+        } else if (v == buy9) {
+            handleBuy9();
+        } else if (v == buy10) {
+            handleBuy10();
+        } else if (v == buy11) {
+            handleBuy11();
     }
 
     private void handleBuy1() {
+        }
+        // Check if the player has enough score to make the purchase
         if (score >= costs[0]) {
+            // Deduct the cost from the score
             score -= costs[0];
+            // Update the text view to display the updated score
             points.setText(String.format("%.2f", score) + " Dining Points");
+            // Increase the multiplier by 0.1
             multiplier += 0.1;
-            costs[0] *= 1.11;
+            // Increase the cost of the item by 15%
+            costs[0] *= 1.15;
+            // Update the text on the button to display the new cost
             buy1.setText("" + String.format("%.2f", costs[0]));
+            // Increase the level of the upgrade by 1
             levels[0]++;
+            // Update the text on the label to display the new level
             name1.setText("Upgrade 1 (" + levels[0] + "x)");
         } else {
+            // Display a toast message indicating that the player doesn't have enough score
             Toast.makeText(Shop.this, "Not enough Dining Points!", Toast.LENGTH_SHORT).show();
         }
     }
@@ -97,8 +150,8 @@ public class Shop extends Activity {
         if (score >= costs[1]) {
             score -= costs[1];
             points.setText(String.format("%.2f", score) + " Dining Points");
-            multiplier += 0.2;
-            costs[1] *= 1.11;
+            multiplier += 0.1;
+            costs[1] *= 1.15;
             buy2.setText("" + String.format("%.2f", costs[1]));
             levels[1]++;
             name2.setText("Upgrade 2 (" + levels[1] + "x)");
@@ -106,4 +159,135 @@ public class Shop extends Activity {
             Toast.makeText(Shop.this, "Not enough Dining Points!", Toast.LENGTH_SHORT).show();
         }
     }
+
+    private void handleBuy3() {
+        if (score >= costs[2]) {
+            score -= costs[2];
+            points.setText(String.format("%.2f", score) + " Dining Points");
+            multiplier += 0.1;
+            costs[2] *= 1.15;
+            buy3.setText("" + String.format("%.2f", costs[2]));
+            levels[2]++;
+            name3.setText("Upgrade 3 (" + levels[2] + "x)");
+        } else {
+            Toast.makeText(Shop.this, "Not enough Dining Points!", Toast.LENGTH_SHORT).show();
+        }
+
+        private void handleBuy4() {
+            if (score >= costs[3]) {
+                score -= costs[3];
+                points.setText(String.format("%.2f", score) + " Dining Points");
+                multiplier += 0.1;
+                costs[3] *= 1.15;
+                buy4.setText("" + String.format("%.2f", costs[3]));
+                levels[3]++;
+                name4.setText("Upgrade 4 (" + levels[3] + "x)");
+            } else {
+                Toast.makeText(Shop.this, "Not enough Dining Points!", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        private void handleBuy5() {
+            if (score >= costs[4]) {
+                score -= costs[4];
+                points.setText(String.format("%.2f", score) + " Dining Points");
+                multiplier += 0.1;
+                costs[4] *= 1.15;
+                buy5.setText("" + String.format("%.2f", costs[4]));
+                levels[4]++;
+                name5.setText("Upgrade 5 (" + levels[4] + "x)");
+            } else {
+                Toast.makeText(Shop.this, "Not enough Dining Points!", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        private void handleBuy6() {
+            if (score >= costs[5]) {
+                score -= costs[5];
+                points.setText(String.format("%.2f", score) + " Dining Points");
+                multiplier += 0.1;
+                costs[5] *= 1.15;
+                buy6.setText("" + String.format("%.2f", costs[5]));
+                levels[5]++;
+                name6.setText("Upgrade 6 (" + levels[5] + "x)");
+            } else {
+                Toast.makeText(Shop.this, "Not enough Dining Points!", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        private void handleBuy7() {
+            if (score >= costs[6]) {
+                score -= costs[6];
+                points.setText(String.format("%.2f", score) + " Dining Points");
+                multiplier += 0.1;
+                costs[6] *= 1.15;
+                buy7.setText("" + String.format("%.2f", costs[6]));
+                levels[6]++;
+                name7.setText("Upgrade 7 (" + levels[6] + "x)");
+            } else {
+                Toast.makeText(Shop.this, "Not enough Dining Points!", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        private void handleBuy8() {
+            if (score >= costs[7]) {
+                score -= costs[7];
+                points.setText(String.format("%.2f", score) + " Dining Points");
+                multiplier += 0.1;
+                costs[7] *= 1.15;
+                buy8.setText("" + String.format("%.2f", costs[7]));
+                levels[7]++;
+                name8.setText("Upgrade 8 (" + levels[7] + "x)");
+            } else {
+                Toast.makeText(Shop.this, "Not enough Dining Points!", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        private void handleBuy9() {
+            if (score >= costs[8]) {
+                score -= costs[8];
+                points.setText(String.format("%.2f", score) + " Dining Points");
+                multiplier += 0.1;
+                costs[8] *= 1.15;
+                buy9.setText("" + String.format("%.2f", costs[8]));
+                levels[8]++;
+                name9.setText("Upgrade 9 (" + levels[8] + "x)");
+            } else {
+                Toast.makeText(Shop.this, "Not enough Dining Points!", Toast.LENGTH_SHORT).show();
+            }
+        }
+        private void handleBuy10() {
+            if (score >= costs[9]) {
+                score -= costs[9];
+                points.setText(String.format("%.2f", score) + " Dining Points");
+                multiplier += 0.1;
+                costs[9] *= 1.15;
+                buy10.setText("" + String.format("%.2f", costs[9]));
+                levels[9]++;
+                name10.setText("Upgrade 10 (" + levels[9] + "x)");
+            } else {
+                Toast.makeText(Shop.this, "Not enough Dining Points!", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        private void handleBuy11() {
+            if (score >= costs[10]) {
+                score -= costs[10];
+                points.setText(String.format("%.2f", score) + " Dining Points");
+                multiplier += 0.1;
+                costs[10] *= 1.15;
+                buy11.setText("" + String.format("%.2f", costs[10]));
+                levels[10]++;
+                name11.setText("Upgrade 11 (" + levels[10] + "x)");
+            } else {
+                Toast.makeText(Shop.this, "Not enough Dining Points!", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+
+
+
 }
+
